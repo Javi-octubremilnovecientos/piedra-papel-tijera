2257a4ee63f62032f29f9e8bf6c59a274def717c
javier lombardero <javier.lombar2@gmail.com>
2024-12-16 13:36:17 +0100

    algo

--- assets/scisros.js
+++ assets/scisros.js
@@ -1,1 +1,25 @@
- 
+ const Imgs = document.querySelectorAll(".Tres img")
+  const Maquina = document.querySelector(".Dos .result #maquina")
+  const Usuario = document.querySelector(".Dos .result #usuario")
+
+  console.log(Usuario)
+Imgs.forEach((img)=>{
+    img.addEventListener("click",(e)=>{
+      let objeto = e.target.id 
+    
+     switch (objeto) {
+        case "piedra":
+             Usuario.style.backgroundImage = "url(../medios/PIEDRA.png)"
+            break;
+        case "papel":
+              Usuario.style.backgroundImage = "url(../medios/PAPEL.png)"
+            break;
+        case "tijera":
+              Usuario.style.backgroundImage = "url(../medios/TIJERA.png)"
+            break;
+     
+        default:
+            break;
+     }
+    })
+})
